cdc206ad04c0d891946f6b334f023faf50160336
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-06-04 19:25:05 +0300

    done refactoring code without libraries

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,27 +1,30 @@
-// const BASE_URL = 'https://api.thecatapi.com/v1';
-// const API_KEY =
-//   'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
+import scss from './sass/index.scss';
+import { renderCard, renderInfo } from './index.js';
+import { refs } from './index';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
 
-// export const fetchBreeds = () => {
-//   return fetch(`https://api.thecatapi.com/v1/breeds`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .catch(error => console.log(error));
-// };
+export const fetchBreeds = () => {
+  return fetch(`${BASE_URL}/breeds`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
 
-// export const fetchCatByBreed = breedId => {
-//   return fetch(
-//     `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-//   )
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .catch(error => console.log(error));
-// };
+export const fetchCatByBreed = breedId => {
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
+};
--- src/index.js
+++ src/index.js
@@ -1,95 +1,62 @@
 import scss from './sass/index.scss';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 const API_KEY =
   'live_YQPtgQjxNvntrxfOyZ3N1MnSlZixBwThs5vJDplQB1hkUEVNvIS1L7Eq7M1b054J';
 
-const refs = {
+export const refs = {
   select: document.querySelector('.breed-select'),
   option: document.querySelector('.option-js'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
-// function fetchBreeds(breed) {
-//   return fetch(`https://api.thecatapi.com/v1/breeds/`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
 
-// fetch('https://api.thecatapi.com/v1/breeds').then(response => {
-//   return response.json();
-// });
+refs.select.addEventListener('change', onSelectChange);
 
-// refs.loader.classList.add('visually-hidden');
 refs.select.classList.add('visually-hidden');
 refs.catInfo.classList.add('visually-hidden');
-fetchBreeds();
 
-function fetchBreeds() {
-  fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      return response.json();
-    })
-    .then(breeds => {
-      refs.loader.classList.add('visually-hidden');
-      refs.select.classList.remove('visually-hidden');
-      renderCard(breeds);
-    })
-    .catch(error => {
-      refs.error.classList.remove('visually-hidden');
-      refs.select.classList.add('visually-hidden');
-      console.log(error);
-    });
-}
+fetchBreeds()
+  .then(breeds => {
+    refs.select.classList.remove('visually-hidden');
+    renderCard(breeds);
+  })
+  .catch(error => {
+    refs.error.classList.remove('visually-hidden');
+    refs.select.classList.add('visually-hidden');
+  })
+  .finally(() => refs.loader.classList.add('visually-hidden'));
 
-function renderCard(breeds) {
+export function renderCard(breeds) {
   refs.select.innerHTML = breeds
     .map(({ id, name }) => {
       return `<option value="${id}" class="option-js">${name}</option>`;
     })
     .join('');
-  // refs.loader.classList.remove('visually-hidden');
 }
 
-function fetchCatByBreed(breedId) {
-  fetch(
-    `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-  )
-    .then(response => {
-      // refs.loader.classList.remove('visually-hidden');
-      return response.json();
-    })
-    .then(breeds => {
-      renderInfo(breeds);
-      refs.catInfo.classList.remove('visually-hidden');
-      refs.loader.classList.add('visually-hidden');
-    })
-    .catch(error => {
-      refs.error.classList.remove('visually-hidden');
-      console.log(error);
-    });
-}
-
-function renderInfo(breeds) {
+export function renderInfo(breeds) {
   refs.catInfo.innerHTML = breeds
     .map(({ url, breeds }) => {
       return `<img src="${url}" width='350' hight='auto'><div class="wrapper"><h2 class="title">${breeds[0].name}</h2><p class="description">${breeds[0].description}</p>
-      <p>Temperament:${breeds[0].temperament}</p></div>`;
+     <span class = "accent">Temperament:</span>  <p> ${breeds[0].temperament}</p></div>`;
     })
     .join('');
 }
 
-refs.select.addEventListener('change', onSelectChange);
 function onSelectChange(e) {
   const breedId = e.target.value;
   refs.loader.classList.remove('visually-hidden');
   refs.catInfo.classList.add('visually-hidden');
   fetchCatByBreed(breedId);
-}
 
-// function renderCard(breeds) {
-//   const markup = `<option value="${breeds.id}">${breeds.name}</option>`;
-//   refs.select.innerHTML = markup;
-// }
+  fetchCatByBreed(breedId)
+    .then(breeds => {
+      renderInfo(breeds);
+      refs.catInfo.classList.remove('visually-hidden');
+    })
+    .catch(error => {
+      refs.error.classList.remove('visually-hidden');
+    })
+    .finally(() => refs.loader.classList.add('visually-hidden'));
+}
